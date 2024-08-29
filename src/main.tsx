ebf10f13fcfc1778be9becb9d4dacf8cb87932a8
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-08-29 18:51:51 +0200

    Implemented React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
+// Import routing
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { route } from './routes/route.tsx'
+
+const router = createBrowserRouter(route)
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //<StrictMode>
+    <RouterProvider router={router} />
+  //</StrictMode>,
 )
---
+++ src/routes/route.tsx
@@ -0,0 +1,9 @@
+import App from "../App";
+
+export const route = [{
+    path: '',
+    element: <App />,
+    children: [
+        
+    ]
+}]
