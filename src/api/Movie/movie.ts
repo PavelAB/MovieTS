d9601eed2d1140d8cb5967c8bd4954d2d6ab49c3
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-09-01 17:32:48 +0200

    Add JSDoc for Movie fetch

--- src/api/Movie/movie.ts
+++ src/api/Movie/movie.ts
@@ -1,6 +1,14 @@
 
 const MOVIE_URL: string = import.meta.env.VITE__MOVIES_API_URL
 
+/**
+ * Fetches a movie by its ID.
+ * 
+ * @param {string} movieID 
+ * @returns {Promise<Object>} 
+ */
+
+// TODO change Object
 export const fetchMovieByID = async (movieID: string) => {
 
     if(!movieID)
--- src/hooks/useMovie.ts
+++ src/hooks/useMovie.ts
@@ -1,7 +1,14 @@
 import { useQuery } from "@tanstack/react-query"
 import { fetchMovieByID } from "../api/Movie/movie"
 
-export const useMovie = (movieID: string) => {
+
+/**
+ * Custom React hook to fetch movie details by movie ID.
+ *
+ * @param {string} movieID - The ID of the movie to fetch.
+ * @returns {Object} An object containing the query status and data.
+ */
+export const useMovieByID = (movieID: string): object => {
     return useQuery({
         queryKey: ['movies', movieID], 
         queryFn: () => fetchMovieByID(movieID)
