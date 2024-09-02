e03d6feca55597749383251f2a8c0659e76ab824
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-09-02 15:29:29 +0200

    small CSS fix in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,40 @@
+
+
+// TODO For small displays, rearrange the list with the links.
+
 const Header = () => {
+
+
+
     return (
-        <div className="m-0 col-span-12 pt-6 flex justify-between items-center border border-red-500">
-            <div>
-                LOGO
+        <div className="m-0 col-span-12 flex justify-between items-center border border-red-500">
+            <div className="flex items-center">
+                <div className="ml-9 mr-9 border border-blue-500 h-10 w-10 flex items-center justify-center">
+                    <p className="border border-black">
+                        LOGO
+                    </p>
+                </div>
+                <div >
+                    <ul className="w-auto flex flex-col sm:flex-row space-x-0 sm:space-x-4">
+                        <li>
+                            <p>Home</p>
+                        </li>
+                        <li>
+                            <p>About</p>
+                        </li>
+                        <li>
+                            <p>Movies</p>
+                        </li>
+                        <li>
+                            <p>Stars</p>
+                        </li>
+                        <li>
+                            <p>Awards</p>
+                        </li>
+                    </ul>
+                </div> 
             </div>
-            <div >
-                <ul className="w-auto flex flex-col sm:flex-row space-x-0 sm:space-x-4">
-                    <li>
-                        <p>Home</p>
-                    </li>
-                    <li>
-                        <p>About</p>
-                    </li>
-                    <li>
-                        <p>Movies</p>
-                    </li>
-                    <li>
-                        <p>Stars</p>
-                    </li>
-                    <li>
-                        <p>Awards</p>
-                    </li>
-                </ul>
-            </div>
+            
             <div>
                 SEARCH BAR
             </div>
--- src/pages/Home/home.page.tsx
+++ src/pages/Home/home.page.tsx
@@ -3,7 +3,7 @@
 const HomePage = () => {
     return (
         <>
-            <h1 className="col-span-12">
+            <h1 className="col-span-12 border border-red-600">
                 Home Page: Movie
             </h1>
         </>
