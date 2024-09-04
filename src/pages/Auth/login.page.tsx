c7fcbf3b9ad060a9fbcf8907022656929b79eb80
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-09-04 16:58:49 +0200

    Added some CSS in login.page.tsx

--- src/pages/Auth/login.page.tsx
+++ src/pages/Auth/login.page.tsx
@@ -7,10 +7,12 @@ const LoginPage: React.FC = () => {
 
 
 
-
+    // TODO CSS needed to be review
     return (
-        <div className="col-span-12 border border-red-600">
-            
+        <div className="col-span-12 border border-red-600 "> 
+            <div className="mx-auto max-w-md mt-40 bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
+
+            </div>
         </div>
     )
 }
