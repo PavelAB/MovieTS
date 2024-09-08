0916a1e42428022fafe2cafb7f79a1f9180603c0
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-09-08 21:52:21 +0200

    Created function in userFormValidators.ts to validate the sign-up form

--- src/Validators/userFormValidators.ts
+++ src/Validators/userFormValidators.ts
@@ -2,7 +2,7 @@ import { ErrorValidator } from "../types/Error";
 import { User } from "../types/User";
 
 /**
- * Function to validate form data.
+ * Function to validate form data from Login.
  *
  * @param {Partial<User>} formData - The object containing the form data.
  * @returns {ErrorValidator} - An object containing a boolean indicating if validation was successful and an error message.
@@ -10,15 +10,53 @@ import { User } from "../types/User";
 export const validateLoginFormData = (formData: Partial<User>): ErrorValidator => {
 
 
-    if(formData.login?.trim() === ""){
+    if(!formData.login?.trim()){
         const message: string = "No login, please try again."
         return { msg: message, state: false }
     }
 
-    if(formData.password?.trim() === ""){
+    if(!formData.password?.trim()){
         const message: string = "No password, please try again."
         return { msg: message, state: false }
     }
+    
+    const message: string = "Validation has been successful."
+    return {msg: message, state: true}
+}
+
+/**
+ * Function to validate form data from Sign up.
+ *
+ * @param {Partial<User>} formData - The object containing the form data.
+ * @returns {ErrorValidator} - An object containing a boolean indicating if validation was successful and an error message.
+ */
+export const validateSignUpFormData = (formData: Partial<User>): ErrorValidator => {
+    
+    if(!formData.first_name?.trim()){
+        const message: string = "No First name, please try again."
+        return { msg: message, state: false }
+    }
+    if(!formData.last_name?.trim()){
+        const message: string = "No Last name, please try again."
+        return { msg: message, state: false }
+    }
+    if(!formData.birth_date){
+        const message: string = "No Birth date, please try again."
+        return { msg: message, state: false }
+    }
+    if(!formData.login?.trim()){
+        const message: string = "No login, please try again."
+        return { msg: message, state: false }
+    }
+    if(!formData.email?.trim()){
+        const message: string = "No email, please try again."
+        return { msg: message, state: false }
+    }
+    if(!formData.password?.trim()){
+        const message: string = "No password, please try again."
+        return { msg: message, state: false }
+    }
+         
     
     const message: string = "Validation has been successful."
     return {msg: message, state: true}
