509b9c8266adaadcacfb78c704094864a5459973
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-09-08 22:35:10 +0200

    Completed API call to fetch sign-up for a new user

--- src/api/User/ApiUser.ts
+++ src/api/User/ApiUser.ts
@@ -5,11 +5,11 @@ const MOVIE_URL: string = import.meta.env.VITE__MOVIES_API_URL
 
 
 /**
- * Handle user login in application
+ * Handles user login in the application.
  * 
- * @param {Partial<User>} userData - Object containing `login` and `password`.
- * @returns {Promise<Movie>} Resolves with the connected User object.
- * @throws {Error} If userData is incomplete or login fails.
+ * @param {Partial<User>} userData - An Object containing `login` and `password`.
+ * @returns {Promise<User>} - A promise that resolves with the connected User object.
+ * @throws {Error} - If userData is incomplete or login fails.
  */
 
 export const login = async (userData: Partial<User>): Promise<User> => {
@@ -34,6 +34,42 @@ export const login = async (userData: Partial<User>): Promise<User> => {
         const errorResponse: ErrorResponse = result as ErrorResponse
         throw new Error(`Error during the POST request: ${errorResponse.msg}`)
     }
+
+    const user: User = result as User
+
+    return user
+}
+
+
+/**
+ * Handles user sign-up in the application.
+ * 
+ * @param {Partial<User>} userData - An object containing the information for sign-up.
+ * @returns {Promise<User>} - A promise that resolves with the connected User object.
+ * @throws {Error} - If userData is incomplete or sign-up fails.
+ */
+
+export const signUp = async (userData: Partial<User>): Promise<User> => {
+
+    if(!userData)
+        throw new Error(`An error has occurred, no data received. Please try again.`)
+
+    const url: string = MOVIE_URL + "/auth/register"
+    const response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+
+    })
+
+    const result: User | ErrorResponse = await response.json()
+
+    if(!response){
+        const errorResponse: ErrorResponse = result as ErrorResponse
+        throw new Error(`Error during the POST request: ${errorResponse.msg}`)
+    } 
 
     const user: User = result as User
 
