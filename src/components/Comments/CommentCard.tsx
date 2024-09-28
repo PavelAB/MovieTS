95ddc2c3cd3718ac67cd53571125b5a7e0f463f8
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-09-28 20:34:27 +0200

    Minor modifications in UserContext.tsx, CommentCard.tsx, and Comment.ts

--- src/components/Comments/CommentCard.tsx
+++ src/components/Comments/CommentCard.tsx
@@ -5,7 +5,7 @@ import { Comment } from "../../types/Comment";
 const CommentCard: React.FC<{Comment: Comment}> = ({Comment: Comment}) => {
     return (
         <div className="bg-white p-4 rounded-lg shadow-md">
-            <h3 className="text-lg font-bold">{Comment.ID_User ? Comment.ID_User : 'Unknown'}</h3>
+            <h3 className="text-lg font-bold">{Comment.UserFirstName ? Comment.UserFirstName : 'Unknown'}</h3>
             <p className="text-gray-700 text-sm mb-2">
                 Posted on {new Date(Comment.createdAt).toLocaleDateString()}
             </p>
--- src/types/Comment.ts
+++ src/types/Comment.ts
@@ -5,6 +5,7 @@ export interface Comment {
     ID_Comment: number;
     body: string;
     ID_User: any; // TODO change this
+    UserFirstName: string;
     
     Movie?: Partial<Movie>;
     createdAt: Date;
