9b6b947b3ffbf9ea891d523bda6c8c0534f7e3d4
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-10-18 17:42:43 +0200

    Create new custom React hook to handle creation or update of a like on a comment, and add documentation

--- src/hooks/useComment.ts
+++ src/hooks/useComment.ts
@@ -1,7 +1,7 @@
-import { useQuery, UseQueryResult } from "@tanstack/react-query"
-import { Comment } from "../types/Comment"
-import { fetchCommentsByMovie } from "../api/Comment/ApiComment"
-import { SuccessResponse } from "../types/SuccesResponse"
+import { useMutation, UseMutationResult, useQuery, UseQueryResult } from "@tanstack/react-query"
+import { Comment, Like } from "../types/Comment"
+import { createLike, fetchCommentsByMovie } from "../api/Comment/ApiComment"
+import { SuccessResponse, SuccessResponseMsg } from "../types/SuccesResponse"
 
 /**
  * Custom React hook to fetch a list of comments based on the provided movie ID.
@@ -23,4 +23,22 @@ export const useCommentByMovie = (ID_Movie: number, token: string, shouldFetch: 
         queryFn: () => fetchCommentsByMovie(ID_Movie, token),
         enabled: shouldFetch
     })
+}
+
+/**
+ * Custom React hook to handle the creation or update of a like on a comment.
+ *
+ * @param {Like} likeData - Object containing the data to create or update a like on a comment.
+ * @param {number} likeData.Comment - The ID of the comment being liked.
+ * @param {number} likeData.User - The ID of the user liking the comment.
+ * @param {string} token - The Bearer token used for authorization in the request.
+ * 
+ * @returns {UseMutationResult<SuccessResponseMsg, Error, void, unknown>} - A mutation result object that includes the mutate function for triggering the request.
+ */
+export const useNewLike = (likeData: Like, token: string):UseMutationResult<SuccessResponseMsg, Error, void, unknown> => {
+    const mutation = useMutation({
+        mutationFn: () => createLike(likeData, token)
+    })
+
+    return mutation
 }
