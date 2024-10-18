6695caa01811c0809aaa5950a694be0203039778
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-10-18 17:45:41 +0200

    Add new Like interface

--- src/types/Comment.ts
+++ src/types/Comment.ts
@@ -1,4 +1,3 @@
-import { MMCommentUser } from "./MMCommentUser";
 import { Movie } from "./Movie";
 import { User } from "./User";
 
@@ -8,6 +7,13 @@ export interface Comment {
     body: string;
     User: Partial<User>; 
     Movie: Partial<Movie>;
-    Comment: MMCommentUser;
+    NumberLikes: number;
+    IDUsersLikes: number[];
     createdAt: Date;
+}
+
+//TODO Check ! I'm not sure if adding a Like interface is a good practice, but since it's a separate table in the database, maybe it's acceptable.
+export interface Like {
+    Comment: number;
+    User: number;
 }
