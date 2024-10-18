34d5c5359684dbce38617b5ebe1bf1dc953a2cb2
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-10-18 17:36:34 +0200

    Implement createLike fetch function and add documentation in ApiComment.ts

--- src/api/Comment/ApiComment.ts
+++ src/api/Comment/ApiComment.ts
@@ -1,9 +1,18 @@
-import { Comment } from "../../types/Comment";
+import { Comment, Like } from "../../types/Comment";
 import { ErrorResponse } from "../../types/Error";
-import { SuccessResponse } from "../../types/SuccesResponse";
+import { SuccessResponse, SuccessResponseMsg } from "../../types/SuccesResponse";
 
 const MOVIE_URL: string = import.meta.env.VITE__MOVIES_API_URL
 
+
+/**
+ * Fetches a list of comments for a given movie, based on the provided `ID_Movie`.
+ * 
+ * @param {number} ID_Movie - The ID of the movie for which the comments are being fetched.
+ * @param {string} token - The Bearer token used for authorization in the request. 
+ * @returns {SuccessResponse<Comment[]>} A promise that resolves to a `SuccessResponse` containing an array of comments for the provided `ID_Movie`.
+ * @throws {Error} If the fetch operation fails or the response contains an error.
+ */
 export const fetchCommentsByMovie = async (ID_Movie: number, token: string): Promise<SuccessResponse<Comment[]>> => {
 
     const url: string = MOVIE_URL + `/comments/params?ID_Movie=${ID_Movie}`
@@ -25,6 +34,39 @@ export const fetchCommentsByMovie = async (ID_Movie: number, token: string): Pro
     const successResponse: SuccessResponse<Comment[]> = result as SuccessResponse<Comment[]>
 
     return successResponse
+}
 
+/**
+ * Function to create or update a like on a comment.
+ * 
+ * @param {Like} likeData - An object containing the data to create or update a like on a comment.
+ * @param {number} likeData.Comment - The ID of the comment being liked.
+ * @param {number} likeData.User - The ID of the user liking the comment.
+ * @param {string} token - The Bearer token used for authorization in the request. 
+ * 
+ * @returns {Promise<SuccessResponseMsg>} A promise that resolves to a 'SuccessResponseMsg' containing information about whether the like was created or updated.
+ * 
+ * @throws {Error} If the creation or update fails, throws an error with a descriptive message.
+ */
+export const createLike = async (likeData: Like, token: string): Promise<SuccessResponseMsg> => {
 
+    const url: string = MOVIE_URL+"/comments/like"
+    const response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(likeData)
+    })
+    const result: SuccessResponseMsg | ErrorResponse = await response.json()
+
+    if(!response.ok) {
+        const errorResponse: ErrorResponse = result as ErrorResponse
+        throw new Error(`Error during the POST request: ${errorResponse.msg}`)
+    }
+
+    const success: SuccessResponseMsg = result as SuccessResponseMsg
+
+    return success
 }
