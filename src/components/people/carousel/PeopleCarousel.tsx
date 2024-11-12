365e4aacb1690be350abb5d8de70f597038f09d1
Pavel <bezukladnikov.pavel.be@gmail.com>
2024-11-12 19:24:08 +0100

    Refactor PeopleCarousel.tsx, first version complete

--- src/components/people/carousel/PeopleCarousel.tsx
+++ src/components/people/carousel/PeopleCarousel.tsx
@@ -6,6 +6,9 @@ import LoaderElement from "../../uiElements/loaderSpin/LoaderElement"
 import IconRight from "../../uiElements/icons/IconRight"
 import IconLeft from "../../uiElements/icons/IconLeft"
 import { useQueryClient } from "@tanstack/react-query"
+import IconDown from "../../uiElements/icons/IconDown"
+import IconUp from "../../uiElements/icons/IconUp"
+import CustomButton from "../../uiElements/buttons/CustomButton"
 
 
 
@@ -107,33 +110,44 @@ const PeopleCarousel: React.FC<{onCenterPersonIdChange: (id: number) => void}> =
 
 
 
-    const PersonCard: React.FC<{ cardPerson: Partial<Person>, cardDimension: string}> = ({ cardPerson, cardDimension}) => {
+    const PersonCard: React.FC<{ dataPerson: Partial<Person>, cardDimension: string}> = ({ dataPerson, cardDimension}) => {
         return (
             <div className={`min-h-[350px] flex flex-col gap-2 items-center justify-center`} style={{ minWidth: cardDimension }}>
                 <img
-                    src={`http://localhost:8080${cardPerson.picture! as string}`}
-                    className={`object-contain`}
+                    src={`http://localhost:8080${dataPerson.picture! as string}`}
+                    className={`object-contain rounded-lg`}
                     style={{ height: cardDimension }}
                 />
                 <div className={`min-h-[50px] overflow-hidden text-center text-ellipsis`} style={{ maxWidth: cardDimension }}>
-                    {cardPerson.ID_Personne}: {cardPerson.first_name} {cardPerson.last_name}
+                    {dataPerson.first_name} {dataPerson.last_name}
                 </div>
             </div>
         )
     }
 
+    const JobFilterButton: React.FC<{buttonJobParams: string}> = ({buttonJobParams}) => {
+        return (
+            <button
+                onClick={() => setCarouselSearchingJob(buttonJobParams)}
+                className={`px-3 h-8 min-w-[85px] text-sm font-medium text-gray-500 ${carouselSearchingJob === buttonJobParams ? 'bg-gray-200' : 'bg-white'} border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}>
+                {buttonJobParams === SEARCHING_JOB_OPTIONS.DEFAULT_EMPTY ? "All" : buttonJobParams}
+            </button>
+
+        )
+    }
+
     return (
-        <div className="flex flex-col min-w-[80%] gap-8  items-center justify-center">
+        <div className="flex flex-col min-w-[80%] border border-red-600 gap-8 items-center justify-center">
             {carouselPeopleArray.length > 0 && 
                 <div className="flex min-w-[100%] items-center justify-center">
                     <button onClick={carouselSwipeLeft}>
                         <IconLeft />
                     </button>
-                    {carouselLeftFarIndex !== null && <PersonCard key={"carouselLeftFarElement"} cardPerson={carouselPeopleArray[carouselLeftFarIndex]} cardDimension="100px" />}
-                    {carouselLeftNearIndex !== null && <PersonCard key={"carouselLeftNearElement"} cardPerson={carouselPeopleArray[carouselLeftNearIndex]} cardDimension="150px" />}
-                    {carouselCenterIndex !== null && <PersonCard key={"carouselCenterElement"} cardPerson={carouselPeopleArray[carouselCenterIndex]} cardDimension="300px" />}
-                    {carouselRightNearIndex !== null && <PersonCard key={"carouselRightNearElement"} cardPerson={carouselPeopleArray[carouselRightNearIndex]} cardDimension="150px" />}
-                    {carouselRightFarIndex !== null && <PersonCard key={"carouselRightFarElement"} cardPerson={carouselPeopleArray[carouselRightFarIndex]} cardDimension="100px" />}
+                    {carouselLeftFarIndex !== null && <PersonCard key={"carouselLeftFarElement"} dataPerson={carouselPeopleArray[carouselLeftFarIndex]} cardDimension="100px" />}
+                    {carouselLeftNearIndex !== null && <PersonCard key={"carouselLeftNearElement"} dataPerson={carouselPeopleArray[carouselLeftNearIndex]} cardDimension="150px" />}
+                    {carouselCenterIndex !== null && <PersonCard key={"carouselCenterElement"} dataPerson={carouselPeopleArray[carouselCenterIndex]} cardDimension="300px" />}
+                    {carouselRightNearIndex !== null && <PersonCard key={"carouselRightNearElement"} dataPerson={carouselPeopleArray[carouselRightNearIndex]} cardDimension="150px" />}
+                    {carouselRightFarIndex !== null && <PersonCard key={"carouselRightFarElement"} dataPerson={carouselPeopleArray[carouselRightFarIndex]} cardDimension="100px" />}
                     <button onClick={carouselSwipeRight}>
                         <IconRight />
                     </button>
@@ -143,62 +157,28 @@ const PeopleCarousel: React.FC<{onCenterPersonIdChange: (id: number) => void}> =
                 <button
                     className="flex px-3 min-w-full border border-gray-500 bg-gray-300 rounded-lg justify-center" 
                     onClick={() => setIsCarouselFiltresOpen((prev) => !prev)}>
-                        <svg 
-                            className="w-[33px] h-[15px] text-gray-800 dark:text-white" 
-                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" 
-                            width="24" 
-                            height="24" 
-                            fill="none" 
-                            viewBox="0 0 24 24">
-                                <path 
-                                    stroke="currentColor" 
-                                    strokeLinecap="round" 
-                                    strokeLinejoin="round" 
-                                    strokeWidth="2" 
-                                    d="m19 9-7 7-7-7"/>
-                        </svg>
-
+                        {isCarouselFiltresOpen ? 
+                            <IconUp /> : 
+                            <IconDown /> 
+                        }
                 </button>
                 {isCarouselFiltresOpen && 
-                    <div className={`${
-                        isCarouselFiltresOpen ? "opacity-100 scale-100" : "opacity-0 scale-95"
-                    } flex items-center justify-center gap-4 transition-all duration-1000 ease-in-out transform`}
-                    style={{
-                        visibility: isCarouselFiltresOpen ? 'visible' : 'hidden',
-                    }}>
-                        <button 
-                            onClick={() => setCarouselSearchingJob(SEARCHING_JOB_OPTIONS.DEFAULT_EMPTY)}
-                            className={`px-3 h-8 min-w-[42px] text-sm font-medium text-gray-500 ${carouselSearchingJob === SEARCHING_JOB_OPTIONS.DEFAULT_EMPTY ? 'bg-gray-200' : 'bg-white'} border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}>
-                            All
-                        </button>
-                        <button 
-                            onClick={() => setCarouselSearchingJob(SEARCHING_JOB_OPTIONS.ACTOR)}
-                            className={`px-3 h-8 min-w-[42px] text-sm font-medium text-gray-500 ${carouselSearchingJob === SEARCHING_JOB_OPTIONS.ACTOR ? 'bg-gray-200' : 'bg-white'} border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}>
-                            Actors
-                        </button>
-                        <button 
-                            onClick={() => setCarouselSearchingJob(SEARCHING_JOB_OPTIONS.WRITER)}
-                            className={`px-3 h-8 min-w-[42px] text-sm font-medium text-gray-500 ${carouselSearchingJob === SEARCHING_JOB_OPTIONS.WRITER ? 'bg-gray-200' : 'bg-white'} border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}>
-                            Directors
-                        </button>
-                        <button 
-                            onClick={() => setCarouselSearchingJob(SEARCHING_JOB_OPTIONS.DIRECTOR)}
-                            className={`px-3 h-8 min-w-[42px] text-sm font-medium text-gray-500 ${carouselSearchingJob === SEARCHING_JOB_OPTIONS.DIRECTOR ? 'bg-gray-200' : 'bg-white'} border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}>
-                            Writters
-                        </button>
+                    <div 
+                        className={`${isCarouselFiltresOpen ? 
+                                "opacity-100 scale-100" : 
+                                "opacity-0 scale-95"
+                                } flex items-center justify-center gap-4 transition-all duration-1000 ease-in-out transform`}
+                        style={{ visibility: isCarouselFiltresOpen ? "visible" : "hidden"}}>
+                            <JobFilterButton buttonJobParams={SEARCHING_JOB_OPTIONS.DEFAULT_EMPTY}/>
+                            <JobFilterButton buttonJobParams={SEARCHING_JOB_OPTIONS.ACTOR}/>
+                            <JobFilterButton buttonJobParams={SEARCHING_JOB_OPTIONS.WRITER}/>
+                            <JobFilterButton buttonJobParams={SEARCHING_JOB_OPTIONS.DIRECTOR}/>                        
                     </div>
                 }
-
-                
-
             </div>
-            <div className="">
-                <button 
-                    className="px-3 h-8 min-w-[78px] text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    onClick={() => queryClient.invalidateQueries({queryKey: ["randomPersons"]})}>
-                        I'm lucky !
-                </button>
-            </div>
+            <CustomButton 
+                onClick={() => queryClient.invalidateQueries({queryKey: ["randomPersons"]})}
+                buttonName="I'm lucky !"/>    
         </div>
 
     )
