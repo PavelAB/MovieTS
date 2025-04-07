afc7c93fb846a58565eec50f192e55c1d0e5465b
Pavel <bezukladnikov.pavel.be@gmail.com>
2025-04-07 16:57:45 +0200

    Initial commit, restart in April 2025

--- src/components/movies/carousel/MovieCarousel.tsx
+++ src/components/movies/carousel/MovieCarousel.tsx
@@ -3,10 +3,12 @@ import { useMoviesByPersonID } from "../../../hooks/useMovie"
 import { Movie } from "../../../types/Movie"
 import { Swiper, SwiperSlide } from "swiper/react"
 
-import { Navigation, Virtual } from "swiper/modules"
+import { Navigation, Pagination, Scrollbar, Virtual } from "swiper/modules"
 
 import 'swiper/css'
 import 'swiper/css/navigation'
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 
 const MovieCarousel: React.FC<{ID_Person: number}> = ({ID_Person}) => {
@@ -14,33 +16,73 @@ const MovieCarousel: React.FC<{ID_Person: number}> = ({ID_Person}) => {
     const shouldFetch: boolean = ID_Person? true : false
     const [carouselMoviesArray, setCaroucelMoviesArray] = useState<Movie[]>([])
 
+    const [slidesPerViewSize, setSlidesPerViewSize] = useState<number>(1)
+
     const {data: moviesByPersonResponse} = useMoviesByPersonID(ID_Person, shouldFetch)
 
     useEffect(()=>{
-        if(moviesByPersonResponse)
+        if(moviesByPersonResponse){
             setCaroucelMoviesArray(moviesByPersonResponse.data)
+        }
     },[moviesByPersonResponse])
+
+    useEffect(()=>{
+        setLengthMovieSwiper()
+    }, [carouselMoviesArray])
+
+    useEffect(() => {
+        updateSlidesPerView()
+        window.addEventListener("resize", updateSlidesPerView)
+    
+        return () => {
+          window.removeEventListener("resize", updateSlidesPerView)
+        }
+    }, [])
+
+    function updateSlidesPerView():void{
+        const width = window.innerWidth
+    
+        if (width >= 1024) {
+            setSlidesPerViewSize(3)
+        } else if (width >= 640) {
+            setSlidesPerViewSize(2)
+        } else {
+            setSlidesPerViewSize(1)
+        }
+      };
+
+    function setLengthMovieSwiper(): void {
+        const width = window.innerWidth
+
+        if(carouselMoviesArray.length > 3 && width >= 1024)
+            setSlidesPerViewSize(3)
+        else
+            setSlidesPerViewSize(carouselMoviesArray.length)
+    }
 
     console.log("length -->", carouselMoviesArray.length, carouselMoviesArray)
     
     return (
-        <div className="border border-red-600">
+        <div className="">
             <Swiper
-                className="w-[350px] h-[350px] border border-blue-600"
-                modules={[Navigation, Virtual]}
+                className="w-[350px] sm:w-[640px] lg:w-[900px] h-[350px]"
+                modules={[Navigation, Pagination, Scrollbar, Virtual]}
                 spaceBetween={50}
-                slidesPerView={1}
+                slidesPerView={slidesPerViewSize}
                 navigation
+                pagination={{
+                    clickable: true
+                }}
                 virtual>
                     {
                         carouselMoviesArray.map((movie: Movie, index) => {
                             return  <SwiperSlide key={movie.ID_Movie} virtualIndex={index} className="items-center">
-                                        <div className="border border-red-700 flex flex-col items-center justify-center gap-10 h-full">
+                                        <div className="flex flex-col items-center justify-center gap-5 h-full">
                                             <img
                                                 src={`http://localhost:8080${movie.cover as string}`}
-                                                className={`object-contain rounded-lg max-h-[100px]`}
+                                                className={`object-contain rounded-lg max-h-[70%]`}
                                             />
-                                            <p className="border border-green-500 flex justify-center">{movie.ID_Movie}</p>
+                                            <p className="flex justify-center px-[10px]">{movie.title}</p>
                                         </div>
                                     </SwiperSlide>
                         })
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,14 +1,14 @@
-import { RouteObject } from "react-router-dom";
-import App from "../App";
-import HomePage from "../pages/home/homePage";
-import MoviePage from "../pages/movies/MoviePage";
-import LoginPage from "../pages/auth/loginPage";
-import SignUpPage from "../pages/auth/signUpPage";
-import AboutPage from "../pages/about/AboutPage";
-import AwardsPage from "../pages/awards/AwardsPage";
-import PeoplePage from "../pages/personnes/PeoplePage";
-import MovieDetailsPage from "../pages/movies/MovieDetailsPage";
-import PersonnesDetailsPage from "../pages/personnes/PersonDetailsPage";
+import { RouteObject } from "react-router-dom"
+import App from "../App"
+import HomePage from "../pages/home/homePage"
+import MoviePage from "../pages/movies/MoviePage"
+import LoginPage from "../pages/auth/loginPage"
+import SignUpPage from "../pages/auth/signUpPage"
+import AboutPage from "../pages/about/AboutPage"
+import AwardsPage from "../pages/awards/AwardsPage"
+import PeoplePage from "../pages/personnes/PeoplePage"
+import MovieDetailsPage from "../pages/movies/MovieDetailsPage"
+import PersonnesDetailsPage from "../pages/personnes/PersonDetailsPage"
 
 export const route: RouteObject[] = [{
     path: '',
